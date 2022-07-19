8429b829f83165798b4f953d29bad9671bb90527
fweber144 <fweber144@protonmail.com>
2022-07-19 21:31:16 +0200

    Update `debug.R`.

--- debug.R
+++ debug.R
@@ -3,6 +3,7 @@
 simresrng <- readRDS(file = "simresrng.rds")
 SIM_ITER <- 4L
 .Random.seed <- simresrng[[SIM_ITER]]
+
 sim_dat_etc <- dataconstructor()
 refm_fit <- fit_ref(dat = sim_dat_etc$dat, fml = sim_dat_etc$fml)
 seed_vs <- sample.int(.Machine$integer.max, 1)
@@ -10,5 +11,9 @@ seed_vs <- sample.int(.Machine$integer.max, 1)
 # debug(projpred:::.get_p_clust)
 # debug(brms:::link_cumulative)
 # debug(brms:::link)
-aug = run_projpred(refm_fit, seed = seed_vs, nclusters_pred = 50, nterms_max = 5)
-lat = run_projpred(refm_fit, seed = seed_vs, latent_proj = TRUE, nclusters_pred = 50, nterms_max = 5)
+projpred_aug <- run_projpred(refm_fit, dat_indep = sim_dat_etc$dat_indep,
+                             seed = seed_vs, nclusters_pred = 50,
+                             nterms_max = 5)
+projpred_lat <- run_projpred(refm_fit, dat_indep = sim_dat_etc$dat_indep,
+                             seed = seed_vs, latent_proj = TRUE,
+                             nclusters_pred = 50, nterms_max = 5)
