da256eecef2681e4e3a07ea862d2ecebce19fbbc
fweber144 <fweber144@protonmail.com>
2022-07-20 10:55:33 +0200

    Update `debug.R` (now: special `d_test$data` element for latent projection).

--- debug.R
+++ debug.R
@@ -14,6 +14,11 @@ seed_vs <- sample.int(.Machine$integer.max, 1)
 projpred_aug <- run_projpred(refm_fit, dat_indep = sim_dat_etc$dat_indep,
                              seed = seed_vs, nclusters_pred = 50,
                              nterms_max = 5)
-projpred_lat <- run_projpred(refm_fit, dat_indep = sim_dat_etc$dat_indep,
+dat_indep_lat <- sim_dat_etc$dat_indep
+dat_indep_lat$Y_orig <- dat_indep_lat$Y
+dat_indep_lat$Y <- colMeans(
+  rstantools::posterior_linpred(refm_fit, newdata = dat_indep_lat)
+)
+projpred_lat <- run_projpred(refm_fit, dat_indep = dat_indep_lat,
                              seed = seed_vs, latent_proj = TRUE,
                              nclusters_pred = 50, nterms_max = 5)
