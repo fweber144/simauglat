8a75dc3b594e712f5455d301c93a5130b7c1667a
fweber144 <fweber144@protonmail.com>
2023-02-22 21:04:16 +0100

    Adjust `table.placement` (default is `"ht"`).

--- sim.R
+++ sim.R
@@ -911,6 +911,7 @@ xtab_perf_at_sgg_indiv <- function(da_info) {
     xtab_obj,
     file = file.path("tabs", fnm_base),
     # caption.placement = "top",
+    table.placement = "!tb",
     include.rownames = FALSE,
     latex.environments = "center",
     sanitize.colnames.function = function(nms) {
